a7019a8d8f02112245f3987afb54d02a0cdf585f
Emad Rihan <emad.rehan.2006@gmail.com>
2026-01-29 03:23:16 -0500

    css roll back

--- script.js
+++ script.js
@@ -1436,8 +1436,7 @@ function displayGamePricesLookup(gameName, gameID, gameDetails, pricesData) {
                                     ${bestPrice.discount > 0 ? `<div class="discount-label">${bestPrice.discount}% off</div>` : ''}
                                 </div>
                                 <div class="price-item">
-                                    <div class="price-label">Save</div>
-                                    <div class="price-value" style="color: var(--success);">${formatPrice(savings)}</div>
+                                    <div class="price-label">Save ${formatPrice(savings)} with Steam</div>
                                 </div>
                             </div>
                         </div>
